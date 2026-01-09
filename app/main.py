f55b71b7cb8f8beb33c0eb8e4d0eca3e19fa288c
Sanyogita45 <sanyogita.24bce10550@vitbhopal.ac.in>
2026-01-09 15:24:49 +0530

    origins

--- app/main.py
+++ app/main.py
@@ -27,11 +27,12 @@ app = FastAPI(title="SkinAware API")
 # 🔥 CORS FIX (MANDATORY)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",   # Vite frontend
-        "http://localhost:3000",
-        "https://skinaware-backend.onrender.com"
-    ],
+     allow_origins=["*"],
+    # allow_origins=[
+    #     "http://localhost:5173",   # Vite frontend
+    #     "http://localhost:3000",
+    #     "https://skinaware-backend.onrender.com"
+    # ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
